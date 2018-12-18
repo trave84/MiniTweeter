a0a2616d19ca314206bc11c3d448a1ee2948a387
trave84 <markerterheaven@gmail.com>
2018-12-18 01:13:09 +0100

    JSON Server + UI done for all CRUD. UI needs more State optimization

--- src/UI.js
+++ src/UI.js
@@ -64,6 +64,52 @@ class UI {
     this.titleInput.value = "";
     this.bodyInput.value = "";
   }
+
+  // FIll form to edit
+  fillForm(data) {
+    this.titleInput.value = data.title;
+    this.bodyInput.value = data.body;
+    this.idInput.value = data.id;
+
+    this.changeFormState("edit");
+  }
+
+  //Clear ID hidden value
+  clearIdInput() {
+    this.idInput.value = "";
+  }
+
+  // Change the form State
+  changeFormState(type) {
+    if (type === "edit") {
+      // Change the state of propertyVar [.post-submit]
+      this.postSubmit.textContent = "Update Post";
+      this.postSubmit.className = "post-submit btn btn-warning btn-block";
+
+      // Create cancel btn
+      const btn = document.createElement("button");
+      btn.className = "post-cancel btn btn-light btn-block";
+      btn.appendChild(document.createTextNode("Cancel Edit"));
+
+      // Get parent
+      const cardForm = document.querySelector(".card-form");
+      // Get elm to insert before
+      const formEnd = document.querySelector(".form-end");
+      cardForm.insertBefore(btn, formEnd);
+    } else {
+      this.postSubmit.textContent = "Posted";
+      this.postSubmit.className = "post-submit btn btn-primary btn-block";
+
+      // Remove cancel btn IF it is there
+      if (document.querySelector(".post-cancel")) {
+        document.querySelector(".post-cancel").remove();
+      }
+      //Clear that ID from hidden field
+      this.clearIdInput();
+      //Clear text
+      this.clearFields();
+    }
+  }
 }
 
 export const ui = new UI();
--- src/app.js
+++ src/app.js
@@ -11,6 +11,12 @@ document.querySelector(".post-submit").addEventListener("click", submitPost);
 //Delete post listener
 document.querySelector("#posts").addEventListener("click", deletePost);
 
+// Edit State listener (via Event Delegation [parent=#post])
+document.querySelector("#posts").addEventListener("click", enableEdit);
+
+// Cancel Edit listener
+document.querySelector(".card-form").addEventListener("click", cancelEdit);
+
 function getPosts() {
   http
     .get("http://localhost:3000/posts")
@@ -21,6 +27,7 @@ function getPosts() {
 function submitPost() {
   const title = document.querySelector("#title").value;
   const body = document.querySelector("#body").value;
+  const id = document.querySelector("#id").value;
 
   // Key = Value hence can use ES6
   const data = {
@@ -28,15 +35,32 @@ function submitPost() {
     body
   };
 
-  //Create a post
-  http
-    .post("http://localhost:3000/posts", data)
-    .then(data => {
-      ui.showAlert("Post added", "alert alert-success");
-      ui.clearFields();
-      getPosts();
-    })
-    .catch(err => console.log(err));
+  // Input Validation
+  if (title === "" || body === "") {
+    ui.showAlert("Please fill in all fields", "alert alert-danger");
+  } else {
+    if (id === "") {
+      //Create a post
+      http
+        .post("http://localhost:3000/posts", data)
+        .then(data => {
+          ui.showAlert("Post added", "alert alert-success");
+          ui.clearFields();
+          getPosts();
+        })
+        .catch(err => console.log(err));
+    } else {
+      //Update the post
+      http
+        .put(`http://localhost:3000/posts/${id}`, data)
+        .then(data => {
+          ui.showAlert("Post updated", "alert alert-success");
+          ui.changeFormState("add");
+          getPosts();
+        })
+        .catch(err => console.log(err));
+    }
+  }
 }
 
 function deletePost(e) {
@@ -56,4 +80,36 @@ function deletePost(e) {
         .catch(err => consolo.log(err));
     }
   }
+  e.preventDefault();
+  ui.clearFields();
+}
+
+// Enable edit State
+function enableEdit(e) {
+  if (e.target.parentElement.classList.contains("edit")) {
+    const postsContainer = document.getElementById("postsContainer");
+    const id = e.target.parentElement.dataset.id;
+    const title = e.target.parentElement.previousElementSibling.textContent;
+    const body = e.target.parentElement.previousElementSibling.textContent;
+
+    const data = {
+      id,
+      title,
+      body
+    };
+
+    // Fill form with current post
+    ui.fillForm(data);
+    postsContainer.scrollIntoView(true);
+    // window.location.hash = ui.titleInput.id;
+  }
+  e.preventDefault();
+}
+
+// Cancel Edit State (via Event Delegation)
+function cancelEdit(e) {
+  if (e.target.classList.contains("post-cancel")) {
+    ui.changeFormState("add");
+  }
+  e.preventDefault();
 }
